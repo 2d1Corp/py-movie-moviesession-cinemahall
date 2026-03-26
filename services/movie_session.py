539479ee377177e2ec653e4ce2d8ad98bc7c703b
Oleksandr Simochko <vhsb721@gmail.com>
2026-03-26 12:03:42 +0100

    'Solution'

--- services/movie_session.py
+++ services/movie_session.py
@@ -52,5 +52,5 @@ def update_movie_session(
     return MovieSession.objects.get(id=session_id)
 
 
-def delete_movie_session_by_id(session_id: int) -> MovieSession:
-    return MovieSession.objects.get(id=session_id).delete()
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.get(id=session_id).delete()
